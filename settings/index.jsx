e3656b8f4777982169fb11797730ab4be3092d56
PeterSumm <peter@summers.aus.net>
2019-01-30 10:16:13 +1100

    Update index.jsx

--- settings/index.jsx
+++ settings/index.jsx
@@ -62,10 +62,15 @@ function mySettings(props) {
         title={<Text bold align="center">Instructions</Text>}>
         <Text>
           This is an HTTP/HTTPS client supporting GET and POST requests (though HTTP only works to IP addresses on your own network).
-          It can be configured with three sets of label, URL, data segment (if using POST) and headers.
-          For example, the label might be 'I need help.' and the other fields contain the request details to send this message to some server. 
+          It is intended for technically proficient audience and use of its more advanced features requires some understanding of JSON 
+          data structures.
+          The app allows configuration with three sets of label, URL, data segment (for POST) and headers. Touching that label will then 
+          fire off the associated request, and the status text returned will be displayed on the watch and can be dismissed by touching it.
+          For example, the label might be 'Send help!' and the URL/data fields contain the request details to send this message to a service 
+          that creates a text message (to which you would need to subscribe). 
           The URL is something like 'https://server.domain.com/path' and the data might look something like {"{'key':'message','value':'Help!'} "} 
           or {"{'key':'message','value':'~Lbl'} "} (where ~Lbl is substitited with the label text).
+          To test your requests, you might want to use the free testing service at https://www.requestcatcher.com.
           The Pebble version of this app could also substitute location-specific information.  I may implement this later if there's enough interest.
         </Text>
       </Section>
